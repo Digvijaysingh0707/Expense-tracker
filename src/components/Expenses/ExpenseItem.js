a54f9614fdfbd3e551750a02aaefcc64545c1e9b
Digvijaysingh0707 <digvijaysinghonly1@gmail.com>
2022-09-13 13:22:46 +0530

    Rendering list of data

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,26 +1,19 @@
-import './ExpenseItem.css'
-import React, { useState } from 'react'
-import ExpenseDate from './ExpenseDate'
-import Card from '../UI/Card'
+import React from 'react';
 
-const ExpenseItem=(props)=>{
-    const [title, setTitle] = useState(props.title)
-    const clickHandler=()=>{
-        setTitle('Update')
-    }
-   
-    return(
-        
+import ExpenseDate from './ExpenseDate';
+import Card from '../UI/Card';
+import './ExpenseItem.css';
 
-        <Card className='expense-item'>
-           <ExpenseDate date={props.date}/>
-            <div className='expense-item__description'>
-                <h2>{title}</h2>
-                <div className='expense-item__price'>Rs {props.amount}</div>
-            </div>
-            <button onClick={clickHandler}>Change title</button>
-        </Card>
-    )
+const ExpenseItem = (props) => {
+  return (
+    <Card className='expense-item'>
+      <ExpenseDate date={props.date} />
+      <div className='expense-item__description'>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
+      </div>
+    </Card>
+  );
 }
 
-export default ExpenseItem
+export default ExpenseItem;
--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,12 +1,11 @@
-import './Card.css'
-function Card(props){
-    const classes = 'card '+ props.className
-    return (
-        <div className={classes}>
-            {props.children}
+import React from 'react';
 
-        </div>
-    )
-}
+import './Card.css';
 
-export default Card
+const Card = (props) => {
+  const classes = 'card ' + props.className;
+
+  return <div className={classes}>{props.children}</div>;
+};
+
+export default Card;
